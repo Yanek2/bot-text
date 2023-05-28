736500ad4d7abd958a4e4658ea0eea47e6f77a94
Yanko <87784331+Yanek2@users.noreply.github.com>
2023-05-28 20:02:20 +0300

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -1,39 +1,51 @@
 import telegram
-from telegram.ext import Updater, MessageHandler, Filters
+from telegram.ext import Updater, CommandHandler
 
-def process_message(update, context):
+# Define your Telegram bot token
+TOKEN = '6289400535:AAHg3UrXFlhQltk5cw8Qi9YvD8l-x_p6dIM'
+
+# Create an instance of the Updater class
+updater = Updater(token=TOKEN, use_context=True)
+dispatcher = updater.dispatcher
+
+# Define a function to handle the /start command
+def start(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Bot started!")
+
+# Define a function to handle incoming messages
+def handle_message(update, context):
+    # Extract the message text
     message_text = update.message.text
-    
-    # Check if the message matches the desired pattern
-    if "Sell entry" in message_text:
-        lines = message_text.split("\n")
-        
-        # Extract relevant information
-        entry = lines[0].split("Sell entry at ")[1]
-        take_profit_1 = lines[1].split("Takeprofit 1 = ")[1]
-        take_profit_2 = lines[2].split("Takeprofit 2 = ")[1]
-        take_profit_3 = lines[3].split("Takeprofit 3 = ")[1]
-        stop_loss = lines[4].split("Stoploss = ")[1]
-        
-        # Format the extracted information
-        response = f"SELL LIMIT EURUSD\nEntry {entry}\nSL {stop_loss}\nTP {take_profit_1}\nTP {take_profit_2}\nTP {take_profit_3}"
-        
-        # Send the formatted message to the user
-        context.bot.send_message(chat_id=update.effective_chat.id, text=response)
 
-def main():
-    # Enter your Telegram bot token here
-    token = '6289400535:AAHg3UrXFlhQltk5cw8Qi9YvD8l-x_p6dIM'
-    updater = Updater(token=token, use_context=True)
-    dispatcher = updater.dispatcher
+    # Parse the message text and extract relevant information
+    entry = ''
+    stoploss = ''
+    takeprofits = []
     
-    # Register the message handler
-    message_handler = MessageHandler(Filters.text, process_message)
-    dispatcher.add_handler(message_handler)
+    lines = message_text.strip().split('\n')
+    for line in lines:
+        if line.startswith('EURUSD Sell entry at'):
+            entry = line.split('=')[1].strip()
+        elif line.startswith('Stoploss ='):
+            stoploss = line.split('=')[1].strip()
+        elif line.startswith('Takeprofit'):
+            parts = line.split('=')
+            takeprofit = parts[1].strip()
+            takeprofits.append(takeprofit)
     
-    # Start the bot
-    updater.start_polling()
-    updater.idle()
+    # Format the extracted information
+    formatted_message = f"SELL LIMIT EURUSD\nEntry {entry}\nSL {stoploss}\n"
+    formatted_message += '\n'.join([f"TP {tp}" for tp in takeprofits])
 
-if __name__ == '__main__':
-    main()
+    # Send the formatted message back to the user
+    context.bot.send_message(chat_id=update.effective_chat.id, text=formatted_message)
+
+# Register the handlers
+start_handler = CommandHandler('start', start)
+message_handler = telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message)
+dispatcher.add_handler(start_handler)
+dispatcher.add_handler(message_handler)
+
+# Start the bot
+updater.start_polling()
+updater.idle()
